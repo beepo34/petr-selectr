2526d3868c6b5b181a296f363212e8a12ae9c242
Aileen Mi <usmiaileen@gmail.com>
2022-11-16 13:55:57 -0800

    create react app

---
+++ petrselectr/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <body>
+        <h1>Petr Selectr</h1>
+      </body>
+    </div>
+  );
+}
+
+export default App;
