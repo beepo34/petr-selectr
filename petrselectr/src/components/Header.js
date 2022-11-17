1512b52142c334e8fe50b73e759e752f017f651f
Teresa Liang <teresaliang2003@gmail.com>
2022-11-17 01:31:18 -0800

    store the value in the search bar in a variable

--- petrselectr/src/components/Header.js
+++ petrselectr/src/components/Header.js
@@ -1,14 +1,13 @@
-import React from 'react';
-import Navbar from 'react-bootstrap/Navbar';
-import Form from 'react-bootstrap/Form';
-import Button from 'react-bootstrap/Button';
+import React, { useState } from 'react';
+import { Navbar, Form, Button } from 'react-bootstrap';
 
 function Header({getData}) {
+    const [courseID, setCourseID] = useState("");
     return (
         <Navbar bg="light" className="mb-3">
             <h1 href="#">PetrSelectr</h1>
-            <Form className="d-flex" onSubmit={getData}>
-                <Form.Control type="text" name="course" placeholder="Search Course" />
+            <Form className="d-flex" onSubmit={(e) => {e.preventDefault(); getData(courseID)}}>
+                <Form.Control type="text" name="course" value={courseID} placeholder="Search Course" onChange={(e)=>{setCourseID(e.target.value)}}/>
                 <Button type="submit" variant="dark">Search</Button>
             </Form>
         </Navbar>
