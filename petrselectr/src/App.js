ea4d4877f292a72d7d57ac64c001d8d0c247208f
Teresa Liang <teresaliang2003@gmail.com>
2022-11-17 19:49:04 -0800

    save course info in a state variable

--- petrselectr/src/App.js
+++ petrselectr/src/App.js
@@ -1,38 +1,40 @@
 import './bootstrap.min.css';
 import React, { useState } from 'react';
-import Header from './components/Header.js'
-import Schedule from './components/Schedule.js'
-import Professors from './components/Professors.js'
+import Header from './components/header/Header.js'
+import Schedule from './components/schedule/Schedule.js'
+import Professors from './components/professors/Professors.js'
 import { courseInfoQuery } from './api/queries/queries'
 import useFetch from 'use-http'
 function App() {
 
   const [profs, setProfs] = useState([{ name: 'shindler', rating: 3.8 }, { name: 'wongma', rating: 2.8 }, { name: 'pattis', rating: 4.8 }, { name: 'thornton', rating: 4.8 }, { name: 'gassko', rating: 1.8 }]);
+  const [courseInfo, setCourseInfo] = useState({});
   const [fall, setFall] = useState([]);
   const [winter, setWinter] = useState([]);
   const [spring, setSpring] = useState([]);
 
   const options = {
     // displays error message when an error occurs
-    onError: ({error}) => {
+    onError: ({ error }) => {
       console.log("An error occurred. Please try searching the course again.")
-    }
+    },
+    responseType: "json"
   }
 
   const graphqlRequest = useFetch('https://api.peterportal.org/graphql/', options) // may be better to put this link in .env?
-  const getCourseInfo = id => graphqlRequest.query(courseInfoQuery, {classID: id})
+  const getCourseInfo = id => graphqlRequest.query(courseInfoQuery, { classID: id })
 
-  const getData = async(id) => {
-  console.log(id) //form input
-    
-  await getCourseInfo(id)
-  console.log(graphqlRequest.data.data.course.id)
+  const getData = async (id) => {
+    console.log(id) //form input
+
+    await getCourseInfo(id)
+    setCourseInfo(graphqlRequest.data.data.course)
   }
 
   return (
     <>
       <Header getData={getData} />
-      <Schedule course={graphqlRequest.data.data.course} fall={fall} winter={winter} spring={spring} />
+      <Schedule course={courseInfo} fall={fall} winter={winter} spring={spring} />
       <Professors profs={profs} />
     </>
   );
